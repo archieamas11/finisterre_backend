4d702987e755f9be783bf633c4f94a3f83005feb
archieamas11 <archiealbarico69@gmail.com>
2025-07-25 18:56:51 +0800

    Refactor CORS handling in authentication scripts to allow dynamic origins and improve preflight response

--- auth/forgot_password.php
+++ auth/forgot_password.php
@@ -1,8 +1,18 @@
 <?php
 // Always send CORS headers
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    $origin = $_SERVER['HTTP_ORIGIN'];
+    if (preg_match('/^http:\\/\\/localhost:\\d+$/', $origin)) {
+        header("Access-Control-Allow-Origin: $origin");
+        header("Access-Control-Allow-Headers: Content-Type");
+        header("Access-Control-Allow-Methods: POST, OPTIONS");
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 
 // Handle preflight OPTIONS request
