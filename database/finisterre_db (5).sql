c8f5bc36b0004a138a4a2e7ceab8c42489474425
archieamas11 <archiealbarico69@gmail.com>
2025-08-06 01:54:03 +0800

    🗑️ chore(database): Remove obsolete finisterre_db (4).sql
    - Deleted the outdated SQL dump file for the database
    - Ensures the repository contains only relevant database files

--- database/finisterre_db (5).sql
+++ database/finisterre_db (5).sql
@@ -3,11 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 04, 2025 at 04:07 PM
+-- Generation Time: Aug 05, 2025 at 05:52 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.16
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,9 +26,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_customers`
 --
 
-CREATE TABLE `tbl_customers` (
-  `customer_id` int NOT NULL,
-  `user_id` int DEFAULT NULL,
+DROP TABLE IF EXISTS `tbl_customers`;
+CREATE TABLE IF NOT EXISTS `tbl_customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `middle_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -44,16 +43,16 @@ CREATE TABLE `tbl_customers` (
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `occupation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_customers`
 --
 
-INSERT INTO `tbl_customers` (`customer_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `email`, `nickname`, `address`, `contact_number`, `birth_date`, `gender`, `religion`, `citizenship`, `status`, `occupation`, `created_at`, `updated_at`) VALUES
-(99, NULL, 'archie', 'amas', 'albarico', 'archiealbarico69@gmail.com', 'chie', 'tunghaan, minglanilla, cebu', '09231226478', '2000-10-24', 'male', 'catholic', 'filipino', 'divorced', 'freelancing', '2025-08-01 14:37:57', '2025-08-01 21:01:55'),
-(109, NULL, 'asd', 'asd', 'albarico', 'archiealbarico69@gmail.com', NULL, 'tunghaan, minglanilla, cebu', '09231226478', '2000-10-24', 'female', 'catholic', 'filipino', 'married', 'freelancing', '2025-08-01 20:43:07', '2025-08-01 20:43:07');
+INSERT INTO `tbl_customers` (`customer_id`, `first_name`, `middle_name`, `last_name`, `email`, `nickname`, `address`, `contact_number`, `birth_date`, `gender`, `religion`, `citizenship`, `status`, `occupation`, `created_at`, `updated_at`) VALUES
+(99, 'archie', 'amas', 'albarico', 'archiealbarico69@gmail.com', 'chie', 'tunghaan, minglanilla, cebu', '09231226478', '2000-10-24', 'male', 'catholic', 'filipino', 'divorced', 'freelancing', '2025-08-01 14:37:57', '2025-08-01 21:01:55');
 
 -- --------------------------------------------------------
 
@@ -61,8 +60,9 @@ INSERT INTO `tbl_customers` (`customer_id`, `user_id`, `first_name`, `middle_nam
 -- Table structure for table `tbl_deceased`
 --
 
-CREATE TABLE `tbl_deceased` (
-  `deceased_id` int NOT NULL,
+DROP TABLE IF EXISTS `tbl_deceased`;
+CREATE TABLE IF NOT EXISTS `tbl_deceased` (
+  `deceased_id` int NOT NULL AUTO_INCREMENT,
   `lot_id` int NOT NULL,
   `dead_fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `dead_gender` enum('male','female') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -76,8 +76,10 @@ CREATE TABLE `tbl_deceased` (
   `dead_birth_date` date NOT NULL,
   `dead_date_death` date NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`deceased_id`),
+  KEY `grave_id` (`lot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_deceased`
@@ -92,25 +94,27 @@ INSERT INTO `tbl_deceased` (`deceased_id`, `lot_id`, `dead_fullname`, `dead_gend
 -- Table structure for table `tbl_lot`
 --
 
-CREATE TABLE `tbl_lot` (
-  `lot_id` int NOT NULL,
+DROP TABLE IF EXISTS `tbl_lot`;
+CREATE TABLE IF NOT EXISTS `tbl_lot` (
+  `lot_id` int NOT NULL AUTO_INCREMENT,
   `customer_id` int NOT NULL,
   `plot_id` int NOT NULL,
   `niche_number` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `niche_status` enum('available','reserved','occupied') COLLATE utf8mb4_general_ci DEFAULT NULL,
   `lot_status` enum('active','completed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`lot_id`),
+  KEY `grave_id` (`plot_id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_lot`
 --
 
 INSERT INTO `tbl_lot` (`lot_id`, `customer_id`, `plot_id`, `niche_number`, `niche_status`, `lot_status`, `created_at`, `updated_at`) VALUES
-(37, 99, 15, '23', 'occupied', 'active', '2025-08-03 07:16:33', '2025-08-03 07:16:33'),
-(38, 109, 1, NULL, NULL, 'active', '2025-08-03 07:16:33', '2025-08-03 07:16:33'),
-(39, 99, 15, '22', 'reserved', 'active', '2025-08-03 16:26:12', '2025-08-03 16:26:12');
+(37, 99, 15, '23', 'occupied', 'active', '2025-08-03 07:16:33', '2025-08-03 07:16:33');
 
 -- --------------------------------------------------------
 
@@ -118,13 +122,16 @@ INSERT INTO `tbl_lot` (`lot_id`, `customer_id`, `plot_id`, `niche_number`, `nich
 -- Table structure for table `tbl_media`
 --
 
-CREATE TABLE `tbl_media` (
-  `media_id` int NOT NULL,
+DROP TABLE IF EXISTS `tbl_media`;
+CREATE TABLE IF NOT EXISTS `tbl_media` (
+  `media_id` int NOT NULL AUTO_INCREMENT,
   `plot_id` int NOT NULL,
   `file_name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`media_id`),
+  KEY `plot_id` (`plot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_media`
@@ -141,8 +148,9 @@ INSERT INTO `tbl_media` (`media_id`, `plot_id`, `file_name`, `created_at`, `upda
 -- Table structure for table `tbl_plots`
 --
 
-CREATE TABLE `tbl_plots` (
-  `plot_id` int NOT NULL,
+DROP TABLE IF EXISTS `tbl_plots`;
+CREATE TABLE IF NOT EXISTS `tbl_plots` (
+  `plot_id` int NOT NULL AUTO_INCREMENT,
   `block` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `category` enum('bronze','silver','platinum','diamond','columbarium','chambers') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `length` varchar(255) DEFAULT NULL,
@@ -152,15 +160,16 @@ CREATE TABLE `tbl_plots` (
   `columns` varchar(255) DEFAULT NULL,
   `status` enum('available','reserved','occupied') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `coordinates` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `coordinates` varchar(255) NOT NULL,
+  PRIMARY KEY (`plot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_plots`
 --
 
 INSERT INTO `tbl_plots` (`plot_id`, `block`, `category`, `length`, `width`, `area`, `rows`, `columns`, `status`, `label`, `coordinates`) VALUES
-(1, 'A', 'diamond', '2.5', '1.2', '3.0', NULL, NULL, 'occupied', NULL, '123.79769285129, 10.249193799482'),
+(1, 'A', 'diamond', '2.5', '1.2', '3.0', NULL, NULL, 'available', NULL, '123.79769285129, 10.249193799482'),
 (2, 'A', 'diamond', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79772218795, 10.249206732589'),
 (3, 'A', 'silver', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79775692256, 10.249221975178'),
 (4, 'A', 'silver', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.7977887235, 10.249236063025'),
@@ -169,7 +178,7 @@ INSERT INTO `tbl_plots` (`plot_id`, `block`, `category`, `length`, `width`, `are
 (7, 'A', 'bronze', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79782322341, 10.249251074665'),
 (8, 'A', 'platinum', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79776900926, 10.249193799482'),
 (9, 'A', 'platinum', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79780116224, 10.249206963537'),
-(10, 'A', 'bronze', NULL, NULL, NULL, NULL, NULL, 'reserved', NULL, '123.79783613154, 10.249222206126'),
+(10, 'A', 'bronze', NULL, NULL, NULL, NULL, NULL, 'available', NULL, '123.79783613154, 10.249222206126'),
 (11, NULL, 'chambers', NULL, NULL, NULL, '5', '5', NULL, NULL, '123.79734521528, 10.24940426143'),
 (12, NULL, 'chambers', NULL, NULL, NULL, '5', '9', NULL, NULL, '123.79737958462, 10.249367524939'),
 (13, NULL, 'chambers', NULL, NULL, NULL, '5', '9', NULL, NULL, '123.79741810198, 10.249323791016'),
@@ -182,120 +191,31 @@ INSERT INTO `tbl_plots` (`plot_id`, `block`, `category`, `length`, `width`, `are
 -- Table structure for table `tbl_users`
 --
 
-CREATE TABLE `tbl_users` (
-  `user_id` int NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `isAdmin` tinyint NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`user_id`, `first_name`, `last_name`, `username`, `password`, `isAdmin`, `created_at`, `updated_at`) VALUES
-(1, 'archie', 'albarico', 'archieamas11', '$2y$10$GMmEGnwoqH6LvEXtvJRg0uXL4AWuU0Vy8gjpjAJg/vH2EhQczroUq', 0, '2025-07-25 08:07:30', '2025-07-25 08:07:30'),
-(2, 'admin', 'admin', 'admin', '$2y$10$MCHAHJhRzwkc1mcck.x5jeP9D0ny5ahQRrmbw5ToQpqWuY7lNbIKW', 1, '2025-07-25 09:00:20', '2025-07-25 09:00:20'),
-(3, 'user', 'user', 'user', '$2y$10$MPkJNQDOu00BLBcgE8hXLOhze9tyJ7UvGKMZDHGUhWImfvbBgKtuS', 0, '2025-07-25 09:23:22', '2025-07-25 09:23:22');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_customers`
---
-ALTER TABLE `tbl_customers`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `tbl_deceased`
---
-ALTER TABLE `tbl_deceased`
-  ADD PRIMARY KEY (`deceased_id`),
-  ADD KEY `grave_id` (`lot_id`);
-
---
--- Indexes for table `tbl_lot`
---
-ALTER TABLE `tbl_lot`
-  ADD PRIMARY KEY (`lot_id`),
-  ADD KEY `grave_id` (`plot_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `tbl_media`
---
-ALTER TABLE `tbl_media`
-  ADD PRIMARY KEY (`media_id`),
-  ADD KEY `plot_id` (`plot_id`);
-
---
--- Indexes for table `tbl_plots`
---
-ALTER TABLE `tbl_plots`
-  ADD PRIMARY KEY (`plot_id`);
-
---
--- Indexes for table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_customers`
---
-ALTER TABLE `tbl_customers`
-  MODIFY `customer_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
-
---
--- AUTO_INCREMENT for table `tbl_deceased`
---
-ALTER TABLE `tbl_deceased`
-  MODIFY `deceased_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
-
---
--- AUTO_INCREMENT for table `tbl_lot`
---
-ALTER TABLE `tbl_lot`
-  MODIFY `lot_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
-
---
--- AUTO_INCREMENT for table `tbl_media`
---
-ALTER TABLE `tbl_media`
-  MODIFY `media_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tbl_plots`
---
-ALTER TABLE `tbl_plots`
-  MODIFY `plot_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT for table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `tbl_users` (`user_id`, `customer_id`, `username`, `password`, `isAdmin`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'archieamas11', '$2y$10$GMmEGnwoqH6LvEXtvJRg0uXL4AWuU0Vy8gjpjAJg/vH2EhQczroUq', 0, '2025-07-25 08:07:30', '2025-07-25 08:07:30'),
+(2, NULL, 'admin', '$2y$10$MCHAHJhRzwkc1mcck.x5jeP9D0ny5ahQRrmbw5ToQpqWuY7lNbIKW', 1, '2025-07-25 09:00:20', '2025-07-25 09:00:20'),
+(3, NULL, 'user', '$2y$10$MPkJNQDOu00BLBcgE8hXLOhze9tyJ7UvGKMZDHGUhWImfvbBgKtuS', 0, '2025-07-25 09:23:22', '2025-07-25 09:23:22');
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `tbl_customers`
---
-ALTER TABLE `tbl_customers`
-  ADD CONSTRAINT `tbl_customers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `tbl_deceased`
@@ -315,6 +235,12 @@ ALTER TABLE `tbl_lot`
 --
 ALTER TABLE `tbl_media`
   ADD CONSTRAINT `tbl_media_ibfk_1` FOREIGN KEY (`plot_id`) REFERENCES `tbl_plots` (`plot_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD CONSTRAINT `tbl_users_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tbl_customers` (`customer_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
